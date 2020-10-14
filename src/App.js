0ee3d257bda3071278936a610f2954a118bbd244
Valentin <qweqweq@qwe>
2020-10-14 21:17:50 +0300

    сделал фильтры

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
 
 import { Header } from './components';
 import { Cart, Home } from './pages';
 import { Route } from 'react-router-dom';
-import { setPizzas } from './redux/actions/pizzas';
 
 import './scss/app.scss';
 
-
 const App = () => {
-    const dispatch = useDispatch();
-
-    
-    React.useEffect(() => {
-        axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-            dispatch(setPizzas(data));
-        });
-    }, []);
-
     return (
         <div className="wrapper">
             <Header />
             <div className="content">
-                <Route path="/" component={Home}  exact />
+                <Route path="/" component={Home} exact />
                 <Route path="/cart" component={Cart} exact />
             </div>
         </div>
